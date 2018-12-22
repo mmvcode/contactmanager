6e062fd0c97fade6369fcafdc2c2b3fc167ca1fc
mmvcode <office@mihaivlasceanu.eu>
2018-12-22 21:19:45 +0200

    Updates

---
+++ precache-manifest.433828178eac7422707e6bea5c7ad8f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "6cb6407c5074953ab6d9",
+    "url": "/contactmanager/static/js/main.6cb6407c.chunk.js"
+  },
+  {
+    "revision": "24f1f25b3bdaf155af3a",
+    "url": "/contactmanager/static/js/1.24f1f25b.chunk.js"
+  },
+  {
+    "revision": "6cb6407c5074953ab6d9",
+    "url": "/contactmanager/static/css/main.77de4901.chunk.css"
+  },
+  {
+    "revision": "24f1f25b3bdaf155af3a",
+    "url": "/contactmanager/static/css/1.44b349d8.chunk.css"
+  },
+  {
+    "revision": "a690dcf4509293123f6108c995373930",
+    "url": "/contactmanager/index.html"
+  }
+];
